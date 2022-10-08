1a5c4050a361ec3ddf56e0b785efc45ce035c886
grayhatgrandpa <grayhatgrandpa@proton.me>
2022-10-08 16:47:20 -0700

    Updated Head.tsx for heroku compatability

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,8 +6,8 @@ const CustomHead: FC = () => {
         <Head>
             <title>Anti Sleep</title>
             <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+                {`@import
+                url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');`}
             </style>
         </Head>
     )
